558e2fefb4d5bb5c34e9493ef7a408713aaf6862
Dev Ojha <dojha@berkeley.edu>
2024-06-20 18:15:57 -0400

    Just resetUpdate Mempool on propose

--- state/execution.go
+++ state/execution.go
@@ -110,6 +110,7 @@ func (blockExec *BlockExecutor) CreateProposalBlock(
 	// Fetch a limited amount of valid txs
 	maxDataBytes := types.MaxDataBytes(maxBytes, evSize, state.Validators.Size())
 
+	blockExec.mempool.ResetUpdate()
 	txs := blockExec.mempool.ReapMaxBytesMaxGas(maxDataBytes, maxGas)
 	block := state.MakeBlock(height, txs, commit, evidence, proposerAddr)
 
