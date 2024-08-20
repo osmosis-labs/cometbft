bda03b3680567fc9be252503cf6963d8b5b0c2ac
ghost <paddymchale@hotmail.com>
2024-08-20 10:30:44 +0100

    fix: comment out expensive debug logs

--- consensus/reactor.go
+++ consensus/reactor.go
@@ -1395,12 +1395,12 @@ func (ps *PeerState) SetHasVoteFromPeer(vote *types.Vote, csHeight int64, valSiz
 }
 
 func (ps *PeerState) setHasVote(height int64, round int32, voteType cmtproto.SignedMsgType, index int32) {
-	ps.logger.Debug("setHasVote",
-		"peerH/R",
-		log.NewLazySprintf("%d/%d", ps.PRS.Height, ps.PRS.Round),
-		"H/R",
-		log.NewLazySprintf("%d/%d", height, round),
-		"type", voteType, "index", index)
+	//	ps.logger.Debug("setHasVote",
+	//		"peerH/R",
+	//		log.NewLazySprintf("%d/%d", ps.PRS.Height, ps.PRS.Round),
+	//		"H/R",
+	//		log.NewLazySprintf("%d/%d", height, round),
+	//		"type", voteType, "index", index)
 
 	// NOTE: some may be nil BitArrays -> no side effects.
 	psVotes := ps.getVoteBitArray(height, round, voteType)
